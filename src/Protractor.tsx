e0c0317ce67d99555ae600ea73a7b89ceb8aa78c
Dylan Alvandi <g.dylanalvandi@gmail.com>
2023-03-07 22:06:11 +0100

    Add basic image upload

--- src/Protractor.tsx
+++ src/Protractor.tsx
@@ -1,9 +1,33 @@
+import { useState } from "react";
+import protractor from './protractor.png'
+
 const Protractor = () => {
+  const [selectedImage, setSelectedImage] = useState<File|null>(null);
   return (
-    <>
-      Protractor
-    </>
-  )
+    <div>
+      {selectedImage && (
+        <div>
+          <img
+            alt="not found"
+            width="1000px"
+            src={URL.createObjectURL(selectedImage)}
+          />
+          <button onClick={() => setSelectedImage(null)}>Remove</button>
+        </div>
+      )}
+
+      <input
+        type="file"
+        name="myImage"
+        onChange={(event) => {
+          if (event.target.files != null) {
+            console.log(event.target.files[0]);
+            setSelectedImage(event.target.files[0]);
+          }
+        }}
+      />
+    </div>
+  );
 }
 
-export default Protractor;
+export default Protractor; 
