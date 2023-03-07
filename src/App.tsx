93e18606a620ce64fe98b94d55a410f8134c935b
Dylan Alvandi <g.dylanalvandi@gmail.com>
2023-03-07 01:33:44 +0100

    Create home page with text and styled images on hover

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,33 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import protractor from './protractor.png'
+
 function App() {
   return (
-    <div className="App">
-      <p>
-        <code>What are you measuring with?</code>
-      </p>
+    <div className="App" style={{
+      backgroundColor: 'steelblue',
+      height: '100vh',
+      width: '100vw',
+      maxHeight: '100vh',
+      maxWidth: '100vw'
+    }}>
+      <div>
+        <div>
+          <p style={{
+            fontWeight: 'bold',
+            fontSize: '4em',
+            padding: '0',
+            margin: '0' 
+            }}> 
+            Pick your fighter! 
+          </p>
+        </div>
+        <div>
+          <img src={protractor} className='tool'/>
+          <img src={protractor} className='tool2'/>
+        </div>
+      </div>
     </div>
   );
 }
