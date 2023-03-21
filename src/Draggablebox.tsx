892ee202c992072b6052023974294ebd637ca11e
Dylan Alvandi <geradylanalvandi@gmail.com>
2023-03-21 21:27:57 +0100

    Access Draggable location

--- src/Draggablebox.tsx
+++ src/Draggablebox.tsx
@@ -1,4 +1,4 @@
-import Draggable from "react-draggable";
+import Draggable, { DraggableData, DraggableEventHandler } from "react-draggable";
 import { useXarrow } from "react-xarrows";
 import generateRandomInteger from "./Tools";
 
@@ -6,11 +6,17 @@ type DraggableBoxProps = {
     id: string
 }
 
-const DraggableBox = ({id} : DraggableBoxProps) => {
-    const updateXarrow = useXarrow();
+const onDragHandler: DraggableEventHandler = (event, data) => {
+    console.log("Point Location: "+data.x+','+data.y)
+}
+
+const DraggableBox = ({ id }: DraggableBoxProps) => {
+
+    useXarrow();
+
     return (
-        <Draggable onDrag={updateXarrow} onStop={updateXarrow} defaultPosition={{x: generateRandomInteger(0, 500), y: generateRandomInteger(0,500)}}>
-            <div id={id} style={{border: 'grey solid 1px', borderRadius: '50%', padding: '0px', textAlign: 'center', width: '1em', height: '1em'}}>
+        <Draggable onDrag={onDragHandler} defaultPosition={{ x: generateRandomInteger(0, 500), y: generateRandomInteger(0, 500) }}>
+            <div id={id} style={{ border: 'grey solid 1px', borderRadius: '50%', padding: '0px', textAlign: 'center', width: '1em', height: '1em' }}>
                 {id}
             </div>
         </Draggable>
